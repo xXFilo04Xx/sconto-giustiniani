1af8b18cecf251a862de68868754953aeceed3b7
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-16 12:20:20 +0200

    Add project files.

---
+++ sconto giustiniani/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sconto_giustiniani
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("inserisci il prezzo totale: ");
+            double totale = double.Parse(Console.ReadLine());
+            double s = totale * 20 / 100;
+            if (totale > 100)
+            {
+                double totalescontato = totale - s;
+                Console.WriteLine($"il prezzo scontato è:{totalescontato} ");
+            }
+            else
+            {
+                Console.WriteLine($"l' importo è: {totale}");
+                Console.ReadLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
